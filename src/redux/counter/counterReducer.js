0d79f32c1823cdc0ee30368ea8f141f389b7b372
engrkashem <kashemaust@gmail.com>
2023-10-30 11:21:38 +0600

    redux useSelector and useDispatch added

--- src/redux/counter/counterReducer.js
+++ src/redux/counter/counterReducer.js
@@ -10,13 +10,13 @@ const counterReducer=(state=initialState, action)=>{
         case INCREMENT:
             return{
                 ...state,
-                value:state.value + 1
+                value:state.value + action.payload
             };
 
         case DECREMENT:
             return{
                 ...initialState,
-                value:state.value - 1
+                value:state.value - action.payload
             }
     
         default:
